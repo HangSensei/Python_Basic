418628cbaa96e97c42b82c5a2232f5f0c42ba8bc
HangSensei <2533858777@qq.com>
2025-05-18 23:16:39 +0800

    Create code05.py

---
+++ Python Basic Grammar/code05.py
@@ -0,0 +1,25 @@
+a = 10
+print(type(a))
+b = 0.5
+print(type(b))
+
+c = "hello"
+print(type(c))
+
+e = 'My name is "xxx"'
+print(type(e))
+# "" and '' together
+f = ''''My 'name' is "xxxx"'''
+print(f)
+
+print(len(f))
+
+a1 = "hello"
+a2 = " world"
+
+print(a1 + a2)
+
+c1 = True
+c2 = False
+print(type(c1))
+print(type(c2))
